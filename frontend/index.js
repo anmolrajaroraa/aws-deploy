6913e5745543ea7e7bdfcf7e69ed28b41e377653
anmolrajaroraa <anmolarora1711@gmail.com>
2019-03-30 11:13:41 +0530

    Frontend code

---
+++ frontend/index.js
@@ -0,0 +1,24 @@
+var express = require('express'),
+  app = express(),
+  port = process.env.PORT || 3000,
+  mongoose = require('mongoose'),
+  Task = require('./api/model/model'), //created model loading here
+  bodyParser = require('body-parser');
+  
+// mongoose instance connection url connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://127.0.0.1:27017/bigbasket'); 
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+var routes = require('./api/routes/routes'); //importing route
+routes(app); //register the route
+
+
+app.listen(port);
+
+
+console.log('todo list RESTful API server started on: ' + port);
